dec2bba3a759f1d00bb006628dfcc26115e5d349
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-02-15 12:23:29 -0500

    StarList and StarWars components added. StarList functionality added, just maps out props.starwarsChars and creates a new <StarWars /> element for each. Also feeds in the necessary props. StarWars functionality added, returns a div which <h1> for the name and <p> for each characteristic.

---
+++ starwars/src/components/StarWars.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './StarWars.css'
+
+const StarWars = props => {
+  return (
+    <div className="character">
+      <h2>{props.name}</h2>
+      <p>Birth Year: {props.birth_year}</p>
+      <p>Created: {props.created}</p>
+      <p>Eye Color: {props.eye_color}</p>
+      <p>Films: {props.films}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Hair Color: {props.hair_color}</p>
+      <p>Height: {props.height}</p>
+      <p>Homeworld: {props.homeworld}</p>
+      <p>Mass: {props.mass}</p>
+      <p>Skin Color: {props.skin_color}</p>
+      <p>Species: {props.species}</p>
+      <p>Starships: {props.starships}</p>
+      <p>URL: {props.url}</p>
+      <p>Vehicles: {props.vehicles}</p>
+    </div>
+  )
+}
+
+export default StarWars
