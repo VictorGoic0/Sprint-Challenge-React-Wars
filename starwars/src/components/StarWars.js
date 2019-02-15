852cfc5518c0b3ac785b5fc9435d6f1bdcd0de13
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-02-15 12:38:17 -0500

    Starships and films overflow fixed by mapping over props.films and creating a new li for each element

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -8,7 +8,9 @@ const StarWars = props => {
       <p>Birth Year: {props.birth_year}</p>
       <p>Created: {props.created}</p>
       <p>Eye Color: {props.eye_color}</p>
-      <p>Films: {props.films}</p>
+      <p>Films: {props.films.map(input => (
+        <li>{input}</li>
+      ))}</p>
       <p>Gender: {props.gender}</p>
       <p>Hair Color: {props.hair_color}</p>
       <p>Height: {props.height}</p>
@@ -16,7 +18,9 @@ const StarWars = props => {
       <p>Mass: {props.mass}</p>
       <p>Skin Color: {props.skin_color}</p>
       <p>Species: {props.species}</p>
-      <p>Starships: {props.starships}</p>
+      <p>Starships: {props.starships.map(input => (
+        <li>{input}</li>
+      ))}</p>
       <p>URL: {props.url}</p>
       <p>Vehicles: {props.vehicles}</p>
     </div>
